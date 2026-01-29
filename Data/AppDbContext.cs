c7ec2118116a9d8658ba9b7efddfc3d5915b9769
levent-bal26 <leventbal26@hotmail.com>
2026-01-30 00:08:32 +0300

    İlk Commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using StudentGradeApi.Models;
+
+namespace StudentGradeApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Sehir> Sehirler => Set<Sehir>();
+    public DbSet<Bolum> Bolumler => Set<Bolum>();
+    public DbSet<Ogrenci> Ogrenciler => Set<Ogrenci>();
+    public DbSet<Ders> Dersler => Set<Ders>();
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using StudentGradeApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// 🔹 Servisler
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// 🔹 DbContext BURAYA EKLENİR
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
+// 🔹 Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
